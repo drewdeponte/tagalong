4ee6cc02da79418f73abb295ccf012bcc8f5a52a
Andrew De Ponte <cyphactor@gmail.com>
2012-05-08 11:36:28 -0700

    Modified the generated migration to be correct.

--- lib/generators/tagalong/migration/templates/active_record/migration.rb
+++ lib/generators/tagalong/migration/templates/active_record/migration.rb
@@ -2,10 +2,31 @@ class TagalongMigration < ActiveRecord::Migration
   def self.up
     create_table :tagalong_tags do |t|
       t.string :name
+      t.integer :number_of_references
+      t.integer :owner_id
+      t.string :owner_type
+
+      t.timestamps
     end
+
+    add_index :tagalong_tags, :owner_id
+    add_index :tagalong_tags, :owner_type
+
+    create_table :tagalong_taggings do |t|
+      t.integer :tagalong_tag_id
+      t.integer :taggable_id
+      t.string :taggable_type
+
+      t.timestamps
+    end
+
+    add_index :tagalong_taggings, :tagalong_tag_id
+    add_index :tagalong_taggings, :taggable_id
+    add_index :tagalong_taggings, :taggable_type
   end
 
   def self.down
     drop_table :tagalong_tags
+    drop_table :tagalong_taggings
   end
 end
