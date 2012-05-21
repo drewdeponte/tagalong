3ad57784e662a6517397b62c1b2275941dd51139
Andrew De Ponte <cyphactor@gmail.com>
2012-05-21 14:04:15 -0700

    Made solr startup wait dynamically.
    I did this because waiting a fixed amount of time didn't seem to be
    working very well for different machines on Travis CI and it was making
    my local runtime take forever. Therefore, I have it check up to fifteen
    times now and as soon as it finds the Solr instance running it stops
    checking and runs the tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,30 @@ require File.expand_path('spec/support/setup_database')
 
 require 'sunspot_solr'
 
+require 'net/http'
+
+def solr_startup_timeout
+  15
+end
+
+def solr_running?
+  begin
+    solr_ping_uri = URI.parse("#{Sunspot.session.config.solr.url}/ping")
+    Net::HTTP.get(solr_ping_uri)
+    true
+  rescue
+    false
+  end
+end
+
+def wait_until_solr_starts
+  (solr_startup_timeout * 10).times do
+    break if solr_running?
+    sleep(0.1)
+  end
+  raise TimeOutError, "Solr failed to start after #{solr_startup_timeout} seconds" unless solr_running?
+end
+
 RSpec.configure do |c|
   c.before(:each) do
     clean_database!
@@ -14,7 +38,7 @@ RSpec.configure do |c|
 
   c.before(:all, :search => true) do
     Sunspot::Solr::Server.new.start
-    sleep 10
+    wait_until_solr_starts
   end
 
   c.before(:each, :search => true) do
